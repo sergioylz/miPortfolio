1bc774f4c61b5a267872ef6dc4cf53b7d2a3a793
Sergio <sergioyihenlin@gmail.com>
2025-11-29 00:48:34 +0100

    act

--- src/components/common/ProjectCard.jsx
+++ src/components/common/ProjectCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function ProjectCard({ project }) {
     return (
-        <div className="bg-gray-50 p-6 h-80 w-100 rounded-xl shadow-lg transition duration-300 ease-in-out hover:shadow-xl hover:translate-y-[-4px] transform">
+        <div className="bg-white-1000 p-6 h-80 w-100 rounded-xl shadow-lg transition duration-300 ease-in-out hover:shadow-xl hover:translate-y-[-4px] transform">
             <a href={project.slug}>
                 <img src={project.image} alt={project.title} className="w-auto h-auto mx-auto object-cover rounded-xl" />
             </a>
